290cdc3aecf059b704991c7c6f722816295053ed
Mehret Kulala <merutaye@gmail.com>
2018-07-12 19:56:55 -0500

    home page routing works

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 
-import AllStuff from '../components/AllStuff/AllStuff';
-import FullStuff from '../components/FullStuff/FullStuff';
-import Login from '../components/Login/Login';
-import MyStuff from '../components/MyStuff/MyStuff';
-import Register from '../components/Register/Register';
+// import AllStuff from '../components/AllStuff/AllStuff';
+// import FullStuff from '../components/FullStuff/FullStuff';
+// import Login from '../components/Login/Login';
+// import MyStuff from '../components/MyStuff/MyStuff';
+// import Register from '../components/Register/Register';
+import Home from '../components/Home/Home';
+
+import Navbar from '../components/Navbar/Navbar';
 
 import './App.css';
 
@@ -12,11 +16,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AllStuff />
+        {/* <AllStuff />
         <FullStuff />
         <Login />
         <MyStuff/>
-        <Register />
+        <Register /> */}
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home} />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
