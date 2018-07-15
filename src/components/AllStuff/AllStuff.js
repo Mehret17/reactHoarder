5179d827ad137ff4ba928e37de253cfbd01578a4
Mehret Kulala <merutaye@gmail.com>
2018-07-14 21:34:11 -0500

    updated allstuffitem

--- src/components/AllStuff/AllStuff.js
+++ src/components/AllStuff/AllStuff.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import allStuffRequests from '../../firebaseRequests/allstuff';
-import authRequests from '../../firebaseRequests/auth';
-import myStuffRequests from '../../firebaseRequests/mystuffs'
+import AllStuffItem from '../AllStuffItem/AllStuffItem';
+// import authRequests from '../../firebaseRequests/auth';
+// import myStuffRequests from '../../firebaseRequests/mystuffs'
 
 import './AllStuff.css';
 
@@ -11,28 +12,29 @@ import './AllStuff.css';
 class AllStuff extends React.Component {
   state = {
     allStuffs : [],
-    myStuff: {},
+    // myStuff: {},
   }
   
   
-  saveStuff = () => {
-    // const newOrder = {...this.state.myStuff}
-    const newStuff = {...this.state.allStuff};
-    newStuff.id = authRequests.getUid();
-    myStuffRequests
-      .postRequest(newStuff)
-      .then(() => {
-        this.props.history.push('/mystuff');
-      })
-      .catch((err) => {
-        console.error('error in mystuff post', err);
-      });
-  }
+  // saveStuff = () => {
+  //   // const newOrder = {...this.state.myStuff}
+  //   const newStuff = {...this.state.allStuff};
+  //   newStuff.id = authRequests.getUid();
+  //   myStuffRequests
+  //     .postRequest(newStuff)
+  //     .then(() => {
+  //       this.props.history.push('/mystuff');
+  //     })
+  //     .catch((err) => {
+  //       console.error('error in mystuff post', err);
+  //     });
+  // }
 
   componentDidMount () {
     allStuffRequests
       .getRequest()
-      .then((allStuffs) => {this.setState({allStuffs});
+      .then((allStuffs) => {
+        this.setState({allStuffs});
   })
       .catch((err) => {
         console.error('error in getting all stuff', err);
@@ -47,14 +49,10 @@ class AllStuff extends React.Component {
 
       // }
       return (
-        <div key={allStuff.id} className="col-sm-3 itemsCard">
-        <div className ="thumbnail">
-        <img className="itemImage"src={allStuff.itemImage} alt=""/>
-        <h3>{allStuff.itemName}</h3>
-        <p>{allStuff.itemDescription}</p>
-        <button className="btn btn-primary"onClick={this.saveStuff}>Add Me</button>
-        </div>
-        </div>
+        <AllStuffItem
+         key={allStuff.id}
+         details={allStuff}
+        />
       );
     });
     return (
--- src/components/AllStuffItem/AllStuffItem.js
+++ src/components/AllStuffItem/AllStuffItem.js
@@ -6,12 +6,13 @@ class AllStuffItem extends React.Component {
   render () {
     const {details} = this.props
       return (
-        <div key={allStuff.id} className="col-sm-3 itemsCard">
+        <div className="col-sm-3 itemCard">
+        {/* <div key={allStuff.id} className="col-sm-3 itemsCard"> */}
         <div className ="thumbnail">
         <img className="itemImage"src={details.itemImage} alt=""/>
         <h3>{details.itemName}</h3>
         <p>{details.itemDescription}</p>
-        <button className="btn btn-primary"onClick={addStuff}>Add Me</button>
+        <button className="btn btn-primary">Add</button>
         </div>
         </div>
       );
